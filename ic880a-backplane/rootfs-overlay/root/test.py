7b484e880ccf42de2f342e2bfea21a551159d69b
Danilo Bargen <mail@dbrgn.ch>
2019-09-02 22:41:57 +0200

    Test script: Always test sensors

--- ic880a-backplane/rootfs-overlay/root/test.py
+++ ic880a-backplane/rootfs-overlay/root/test.py
@@ -31,9 +31,9 @@ print('=== iC880A Backplane Test Program ===\n')
 
 print('Turning on LEDs...')
 g.output(LED_R, g.HIGH)
-time.sleep(0.5)
+time.sleep(0.2)
 g.output(LED_Y, g.HIGH)
-time.sleep(0.5)
+time.sleep(0.2)
 g.output(LED_B, g.HIGH)
 
 print('\nWaiting for button...')
@@ -47,37 +47,34 @@ g.output(LED_R, g.LOW)
 g.output(LED_Y, g.LOW)
 g.output(LED_B, g.LOW)
 
-if input('\nTest sensors? [y/n] ').strip() == 'y':
+print('\nInitializing SHTC3 sensor...')
+shtcx.init()
 
-    print('\nInitializing SHTC3 sensor...')
-    shtcx.init()
+print('Reading sensor values...')
+(temp, humi) = shtcx.read()
+print('  Temperature: {} C'.format(temp))
+print('  Humidity: {} %RH'.format(humi))
 
-    print('Reading sensor values...')
-    (temp, humi) = shtcx.read()
-    print('  Temperature: {} C'.format(temp))
-    print('  Humidity: {} %RH'.format(humi))
+print('Initializing ADC...')
+config = adc.START_CONVERSION | adc.CONVERSION_MODE_ONESHOT \
+    | adc.SAMPLE_RATE_15SPS | adc.PGA_GAIN_1
+bus.write_byte(ADC_DEVICE_ADDRESS, config)
 
-    print('Initializing ADC...')
-    config = adc.START_CONVERSION | adc.CONVERSION_MODE_ONESHOT \
-        | adc.SAMPLE_RATE_15SPS | adc.PGA_GAIN_1
-    bus.write_byte(ADC_DEVICE_ADDRESS, config)
+# Wait a bit for the measurement to finish
+time.sleep(0.15)
 
-    # Wait a bit for the measurement to finish
-    time.sleep(0.15)
+# Read measurement
+data = bus.read_i2c_block_data(ADC_DEVICE_ADDRESS, 0x00, 3)
+value = (data[0] << 8) + data[1]
 
-    # Read measurement
-    data = bus.read_i2c_block_data(ADC_DEVICE_ADDRESS, 0x00, 3)
-    value = (data[0] << 8) + data[1]
+# Calculate voltage for the specified bit of accuracy
+def get_voltage(measurement, bit):
+    v2 = measurement * adc.ADC_REF / (2**bit)
+    total_r = ADC_DIVIDER_R1 + ADC_DIVIDER_R2 + ADC_DIVIDER_R3
+    return v2 / (ADC_DIVIDER_R2 / total_r) * 2
 
-    # Calculate voltage for the specified bit of accuracy
-    def get_voltage(measurement, bit):
-        v2 = measurement * adc.ADC_REF / (2**bit)
-        total_r = ADC_DIVIDER_R1 + ADC_DIVIDER_R2 + ADC_DIVIDER_R3
-        return v2 / (ADC_DIVIDER_R2 / total_r) * 2
-
-    print('  Value is %d/%d' % (value, 2**16))
-    print('  Voltage is %.4f V' % (get_voltage(value, 16) / 1000))
-
+print('  Value is %d/%d' % (value, 2**16))
+print('  Voltage is %.4f V' % (get_voltage(value, 16) / 1000))
 
 print('\n=== DONE! ===')
 
