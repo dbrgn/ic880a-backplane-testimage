489ded473230180a3cf64620c33dfc0f058394c7
Danilo Bargen <mail@dbrgn.ch>
2019-09-02 22:57:23 +0200

    Launch test script on login

--- ic880a-backplane/rootfs-overlay/etc/profile.d/motd.sh
+++ ic880a-backplane/rootfs-overlay/etc/profile.d/motd.sh
@@ -1,5 +1,5 @@
 echo ''
 echo '=*=*= Welcome to the ic880a backplane test image! =*=*='
 echo ''
-echo 'Simply use the `t` command to start the tests.'
+echo 'Use the `t` command to start the tests.'
 echo ''
---
+++ ic880a-backplane/rootfs-overlay/root/.bashrc
@@ -0,0 +1,1 @@
+echo "[bashrc] Starting test script..." && t
